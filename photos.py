5b3c688f32d72d3d93fe7d1367c02985294f7b2f
Maxime Franc <maxime.franc1@gmail.com>
2024-12-16 21:22:28 +0100

    i have an index and a show so i'm happy

--- photos.py
+++ photos.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/photos")
+def photos():
+    return "<p>Pictures there in the futre</p>"
+
+@app.route("/photo/<int:id>")
+def show_photo(id):
+    return f"<p>Photo n° {escape(id)}</p>"

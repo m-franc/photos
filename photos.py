996b8d92bb60a2ab8ef6544ac25f664b6d0b0f53
Maxime Franc <maxime.franc1@gmail.com>
2024-12-16 21:55:42 +0100

    add static/styles.css, but yes, we are in a routing branch. So url build in the main file

--- photos.py
+++ photos.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from markupsafe import escape
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "<p>Hello, World!</p>"
 
 @app.route("/photos")
 def photos():
     return "<p>Pictures there in the futre</p>"
 
-@app.route("/photo/<int:id>")
+@app.route("/photos/<id>")
 def show_photo(id):
     return f"<p>Photo n° {escape(id)}</p>"
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('photos'))
+    print(url_for('show_photo', id=":id"))

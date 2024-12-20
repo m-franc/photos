4fc9ed9242cd309aa4eb29b09b5bdaa9a35bdf5b
Maxime Franc <maxime.franc1@gmail.com>
2024-12-20 16:22:20 +0100

    auth BP created andd add to the app factory. All auth's method made. Next the view !

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from . import db
+from . import auth
 
 
 def create_app(test_config=None):
@@ -29,5 +30,6 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
     db.init_app(app)
+    app.register_blueprint(auth.bp)
 
     return app

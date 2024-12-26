9207082dd3cb0e9a7e3e1cff505188a7e3157c8f
Maxime Franc <maxime.franc1@gmail.com>
2024-12-26 21:57:08 +0100

    upload pictures works ! very interesteing. Next, scraping pictures datas

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from . import auth, blog
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
@@ -23,7 +24,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
 
     db.init_app(app)
     app.register_blueprint(auth.bp)

0296e4bea25f5bce61130aaf0ca55f71b9746631
Maxime Franc <maxime.franc1@gmail.com>
2024-12-27 22:56:12 +0100

    fix authorisation error in modifying get_picture. With a boolean if we want to go go on the show simply, or on delete or update...

--- app/blog.py
+++ app/blog.py
@@ -67,7 +67,7 @@ def create():
             return redirect(url_for('blog.index'))
     return render_template('blog/create.html')
 
-def get_picture(id, check_author=True):
+def get_picture(id, show, check_author=True):
     picture = get_db().execute(
         'SELECT p.id, title, description, path, created, author_id, username'
         ' FROM picture p JOIN user u ON p.author_id = u.id'
@@ -76,21 +76,21 @@ def get_picture(id, check_author=True):
     ).fetchone()
     if picture is None:
         abort(404, f"Picture id {id} doesn't exist.")
-    # if check_author and picture['author_id'] != g.user['id']:
-    #     abort(403)
+    if show == 0:
+        if check_author and picture['author_id'] != g.user['id']:
+            abort(403)
     return picture
 
 @bp.route('/<int:id>', methods=['GET'])
 def show(id):
-    db = get_db()
-    picture = get_picture(id)
+    picture = get_picture(id, 1)
     return render_template('blog/show.html', picture=picture)
 
 
 @bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 def update(id):
-    picture = get_picture(id)
+    picture = get_picture(id, 0)
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
@@ -118,7 +118,7 @@ def update(id):
 @bp.route('/<id>/delete', methods=['POST'])
 @login_required
 def delete(id):
-    get_picture(id)
+    get_picture(id, 0)
     db = get_db()
     db.execute('DELETE FROM picture WHERE id = ?', (id,))
     db.commit()

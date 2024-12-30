9a2e5b05e5a34f1c92b2196657668efafb009b7e
Maxime Franc <maxime.franc1@gmail.com>
2024-12-30 18:35:41 +0100

    alright npm run dev its better than npm run start hehe

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           >
             Read our docs
           </a>
-          <h1 className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44">BONJOUR </h1>
+          <h1 className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44">BONJOUR OKAY JKNFBECOOL </h1>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">

99f817aa217cdab72305398022f7758392ab4906
Maxime Franc <maxime.franc1@gmail.com>
2025-01-04 00:26:44 +0100

    add api fetched for create. make function onSubmit async, modifying setValue and input with my own. Some test not satisfying, but have a nice week-end

--- frontend/src/app/pictures/new/page.tsx
+++ frontend/src/app/pictures/new/page.tsx
@@ -1,12 +1,13 @@
+'use client'
+
 import * as React from "react"
 import { useForm } from "react-hook-form"
 
-
 type FormData = {
-  firstName: string
-  lastName: string
+  title: string,
+  description: string,
+  path: string
 }
-
 
 export default function App() {
   const {
@@ -15,21 +16,35 @@ export default function App() {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>()
-  const onSubmit = handleSubmit((data) => console.log(data))
+  const onSubmit = handleSubmit( async (data) => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/create', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+      console.log('Réponse :', await response.json());
+    } catch (error) {
+      console.error('Erreur :', error);
+    }
+    console.log(data)
+  });
   // firstName and lastName will have correct type
 
-
   return (
-    <form onSubmit={onSubmit}>
-      <label>First Name</label>
-      <input {...register("firstName")} />
-      <label>Last Name</label>
-      <input {...register("lastName")} />
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
+      <label>Titre</label>
+      <input {...register("title")} />
+      <label>Description</label>
+      <input {...register("description")} />
+      <label>Photo</label>
+      <input type="file" {...register("path")} />
       <button
-        type="button"
+        type="submit"
         onClick={() => {
-          setValue("lastName", "luo") // ✅
-          setValue("firstName", true) // ❌: true is not string
+          setValue("title", "value") // ✅
+          setValue("description", "value")
+          setValue("path", "value") // ❌: true is not string
           errors.bill // ❌: property bill does not exist
         }}
       >

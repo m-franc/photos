8f2c4818d0e8fd93396f0c10dc245d4e60027a34
Maxime Franc <maxime.franc1@gmail.com>
2025-02-09 01:18:00 +0100

    delete perfeclty, BUT more user direction will be better (you cant delete if you dont be the owner, perfect but a JS alert will be cool)

---
+++ frontend/src/app/pictures/[id]/delete/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useEffect } from 'react';
+import { redirect } from 'next/navigation'
+import { useParams } from 'next/navigation'
+
+export default function App() {
+
+  const params = useParams<{ id: string }>()
+  // const userId = useAppSelector((state) => state.auth.id); will be use letter to verifying if the user is the author of the picture !
+  useEffect(() => {
+    try {
+      const response = fetch(`http://localhost:5000/${params.id}/delete`, {
+        method: 'POST',
+        body: params.id,
+        credentials: 'include'
+      });
+      console.log(response);
+    } catch (error) {
+      console.error('Erreur :', error);
+    }
+    redirect(`/pictures/`)
+
+  })
+};
+  // firstName and lastName will have correct type

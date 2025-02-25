2da7a0c9da43aee9a023131846b147dd9d293e76
Maxime Franc <maxime.franc1@gmail.com>
2025-02-25 16:47:04 +0100

    cleaning finished so switch on udate db schema for metadata

--- backend/blog.py
+++ backend/blog.py
@@ -75,7 +75,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bp.route('/create', methods=['GET', 'POST'])
-# @login_required
+@login_required
 def create():
     if request.method == 'POST':
 

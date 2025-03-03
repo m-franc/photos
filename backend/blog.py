a6c9f96d1f03307fde6d45471b68b510f81555b3
Maxime Franc <maxime.franc1@gmail.com>
2025-03-03 23:56:23 +0100

    pictures on index are now sorted by brightness omg, or date, or aperture haha

--- backend/blog.py
+++ backend/blog.py
@@ -62,7 +62,8 @@ def index():
     pictures = db.execute(
         'SELECT p.id, title, description, created, path, author_id, username'
         ' FROM picture p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' JOIN metadata m ON p.id = m.picture_id'
+        ' ORDER BY m.date DESC'
     )
 
     # Create the columns of the json statam
@@ -83,7 +84,6 @@ def allmetadatas():
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 def insert_metadata(picture_id, metadata):
     print("PICTURE ID OOOH : ", picture_id)

dca233d8339892b1d76e6bd9cc270722db48ed58
Maxime Franc <maxime.franc1@gmail.com>
2025-03-18 22:24:01 +0100

    fix some urls calls, add docker domain in cors headers

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -21,7 +21,7 @@ export default function App() {
   const dispatch = useAppDispatch();
   const onSubmit = handleSubmit( async (data) => {
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+      const response = await fetch('http://backend:8000/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
